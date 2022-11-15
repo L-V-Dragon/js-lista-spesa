25b60c7baa161b45954f6b15a126cac258749d85
L-V-Dragon <viotti37@gmail.com>
2022-11-15 15:43:14 +0100

    Es completato no bonus

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let lista = ["Uova", "Latte", "Cereali", "Carne", "Pizza", "Pane", "Sdrogo"];
 
-/*let i = 0;
-
-while(i < lista.length){
-    console.log(lista[i]);
-    i++
-}*/
 
 for(let i = 0; i < lista.length; i++){
     console.log(lista[i]);
     document.getElementById("listaStamp").innerHTML += '<li id = "elementoL">'+ lista[i] +'</li>';
 
 }
+
+/*let i = 0;
+
+while(i < lista.length){
+    console.log(lista[i]);
+    i++
+}*/
