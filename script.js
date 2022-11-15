2b8e2e3caf3cca9f08596055a92e0c10c17c298b
L-V-Dragon <viotti37@gmail.com>
2022-11-15 15:10:53 +0100

    Ciclo for completato

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let lista = ["Uova", "Latte", "Cereali", "Carne", "Pizza", "Pane", "Sdrogo"];
 
-let i = 0;
+/*let i = 0;
 
 while(i < lista.length){
     console.log(lista[i]);
     i++
+}*/
+
+for(let i = 0; i < lista.length; i++){
+    console.log(lista[i]);
 }
